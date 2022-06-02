70b3e26bb5c52f87583dc7a87925b9eed3ed00e7
1540600648 <96062868+1540600648@users.noreply.github.com>
2022-06-02 17:12:12 +0800

    pullcommit

--- src/main/java/com/atguigu/git/Gitdemo.java
+++ src/main/java/com/atguigu/git/Gitdemo.java
@@ -9,6 +9,7 @@ public class Gitdemo {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("master22222222222");
+        System.out.println("pull");
     }
 
 }
