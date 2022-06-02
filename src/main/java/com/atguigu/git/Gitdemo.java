3ea8187fac00e3310fc2137604f785b4862f6c28
qcwy <qcwy@qq.com>
2022-06-02 13:24:56 +0800

    second commit

--- src/main/java/com/atguigu/git/Gitdemo.java
+++ src/main/java/com/atguigu/git/Gitdemo.java
@@ -2,7 +2,8 @@ package com.atguigu.git;
 
 public class Gitdemo {
     public static void main(String[] args) {
-        System.out.println("hellogit!!!!!");
+        System.out.println("hellogit!!!!!reset 这种方式会直接把 HEAD 以及 MASTER 一起拉回那个版本，" +
+                "并且会丢失目标版本之后的内容，虽然可以通过 reflog 找到后面的版本，但是这种方式比较强烈的，可能会造成数据丢失");
     }
 
 }
